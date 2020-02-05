2f52da1d8f381bf1f0b349ae79fbbd2a6c749462
terrence <Terrence@wemer.nl>
2020-02-05 12:32:49 +0100

    fixed jumping and gravity, added a model for portals HOTFIX

--- Terrence And Mick Game/Assets/Scripts/Player_Controller.cs
+++ Terrence And Mick Game/Assets/Scripts/Player_Controller.cs
@@ -7,28 +7,31 @@ public class Player_Controller : MonoBehaviour
     public BoxCollider box;
     public CharacterController controller;
     static Animator animator;
+    private Rigidbody rb;
+    private SphereCollider sc;
 
     public float rotationspeed = 100.0f;
     public float speed = 10f;
-    public float gravity = -20f;
     public float jumpHeight = 2f;
 
-    //public Transform groundCheck;
-    //public float groundDistance = 0.4f;
-    //public LayerMask groundMask;
-    //bool isGrounded;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = this.gameObject.GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        sc = GetComponent<SphereCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
         GetComponent<Rigidbody>().AddForce(Physics.gravity, ForceMode.Acceleration);
-        //isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         float translation = Input.GetAxis("Vertical")* speed;
         float rotation = Input.GetAxis("Horizontal") * rotationspeed;
@@ -37,6 +40,10 @@ public class Player_Controller : MonoBehaviour
         transform.Translate(0, 0, translation);
         transform.Rotate(0, rotation, 0);
 
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+        }
         
 
         if (translation == 0)
@@ -70,7 +77,6 @@ public class Player_Controller : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             animator.SetTrigger("IsJumping");
-            
         }
     }
 }
