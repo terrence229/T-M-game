362f3b298ff87f6d2ea2c2b9ba530741b5fb315a
Mick Bogaard <mickbogaard@gmail.com>
2020-02-05 16:27:50 +0100

    Added SceneSwitcher
    Added sceneswitcher animations and code

---
+++ Terrence And Mick Game/Assets/Scripts/SwitchScene/SceneSwitcherCode/FadeInAndOut.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInAndOut : MonoBehaviour
+{
+    public Animator animator;
+    private SceneSwitcher _sceneSwitcher;
+    public string nextScene;
+
+    public void Start()
+    {
+        _sceneSwitcher = new SceneSwitcher();
+    }
+
+    public void FadeToLevel(string nextSceneName)
+    {
+        //saves the name of the next scene in a variable
+        nextScene = nextSceneName;
+
+        //trigger to play animation
+        animator.SetTrigger("FadeAway");
+    }
+
+    public void OnFadeComplete() //AnimationEvent after fade out animation finishes
+    {
+        //switch scene
+        _sceneSwitcher.LoadScene(nextScene);
+    }
+}
