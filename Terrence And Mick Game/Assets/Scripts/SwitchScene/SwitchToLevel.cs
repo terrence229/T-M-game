13c4f3056ec76678652c1b5ad94c9cbecbecc353
Mick Bogaard <mickbogaard@gmail.com>
2020-02-05 16:34:41 +0100

    Small level changer tweak
    changed the level changer a bit, so that we can have everything in 1 script instead of having a script for every single schene change

---
+++ Terrence And Mick Game/Assets/Scripts/SwitchScene/SwitchToLevel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchToLevel : MonoBehaviour
+{
+    private FadeInAndOut _fade;
+
+    public void Start()
+    {
+        _fade = GameObject.Find("FadeAway").GetComponent<FadeInAndOut>();
+    }
+
+    //switch to 2d platformer
+    public void To2DPlatformer()
+    {
+        _fade.FadeToLevel("2DPlatformer");
+    }
+
+    //switch to shooter
+    public void ToShooter()
+    {
+        _fade.FadeToLevel("Shooter");
+    }
+
+
+    //if collision with portal, To2DPlatformer()/shooter() etc.........
+}
